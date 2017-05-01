dbb8dfee71bc9ff82ee6a7692da21ef35d9c5af6
ypj <ypj2005@126.com>
2017-05-01 23:33:42 +0800

    other mistake

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,7 @@ class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
 
   def index
-    @jobs = case params[:id]
+    @jobs = case params[:order]
             when 'by_lower_bound'
               Job.published.order('wage_lower_bound DESC')
             when 'by_upper_bound'
