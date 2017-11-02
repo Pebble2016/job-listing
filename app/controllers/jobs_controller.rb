39d30c2eb8d1be6d765fb9c02596b056954c5a91
ypj <ypj2005@126.com>
2017-11-02 11:34:02 +0800

    add some label text

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -16,7 +16,7 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
 
     if @job.is_hidden
-      flash[:warning] = "This Job already archived"
+      flash[:warning] = "This Store already archived"
       redirect_to root_path
     end
   end
